6c36deafbdcd27b2c24f84ff0b329a5309d181f6
Nancy <52735640+nwkaburu@users.noreply.github.com>
2019-09-25 22:29:47 +0300

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# **Capstone project**
+# 
+# I am using this Jupyter Notebook for the capstone project
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[1]:
+
+
+print("Hello Capstone Project Course!")
+
